bd5abbc904c5674d9183b5514bc38239d4714282
aryadana <rizaldi220399@gmail.com.com>
2019-10-08 10:32:35 +0700

    listener percobaan fix

--- app/src/main/java/id/ac/polinema/idealbodyweight/MainActivity.java
+++ app/src/main/java/id/ac/polinema/idealbodyweight/MainActivity.java
@@ -2,38 +2,31 @@ package id.ac.polinema.idealbodyweight;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import id.ac.polinema.idealbodyweight.fragments.AboutFragment;
-import id.ac.polinema.idealbodyweight.fragments.BmiFragment;
 import id.ac.polinema.idealbodyweight.fragments.BrocaIndexFragment;
 import id.ac.polinema.idealbodyweight.fragments.MenuFragment;
-import id.ac.polinema.idealbodyweight.fragments.ResultFragment;
 
-public class MainActivity extends AppCompatActivity implements MenuFragment.OnFragmentInteractionListener
-		, BrocaIndexFragment.OnFragmentInteractionListener,
-		ResultFragment.OnFragmentInteractionListener,
-		BmiFragment.OnFragmentInteractionListener{
+public class MainActivity extends AppCompatActivity implements
+		MenuFragment.OnFragmentInteractionListener,
+BrocaIndexFragment.OnFragmentInteractionListener{
+
+
 	private AboutFragment aboutFragment;
 	private MenuFragment menuFragment;
 	private BrocaIndexFragment brocaIndexFragment;
-	private ResultFragment resultFragment;
-	private BmiFragment bmiFragment;
-	// Deklarasikan atribut Fragment di sini
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		aboutFragment = AboutFragment.newInstance("Muhammad Nizar Firmansyah");
+		aboutFragment = aboutFragment.newInstance("Rizaldi Aryadana");
 		menuFragment = new MenuFragment();
 		brocaIndexFragment = new BrocaIndexFragment();
-		resultFragment = new ResultFragment();
-		bmiFragment = new BmiFragment();
 		getSupportFragmentManager().beginTransaction()
 				.replace(R.id.fragment_container, menuFragment)
 				.commit();
@@ -48,24 +41,16 @@ public class MainActivity extends AppCompatActivity implements MenuFragment.OnFr
 	@Override
 	public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 		// TODO: Tambahkan penanganan menu di sini
-
 		if (item.getItemId() == R.id.menu_about) {
 			getSupportFragmentManager().beginTransaction()
 					.replace(R.id.fragment_container, aboutFragment)
 					.addToBackStack(null)
 					.commit();
 		}
+
 		return super.onOptionsItemSelected(item);
 	}
 
-	@Override
-	public void onCalculateBrocaIndexClicked(float index) {
-		resultFragment.setInformation(String.format("Your ideal weight is %.2f kg", index));
-		resultFragment.setStatus("1");
-		getSupportFragmentManager().beginTransaction()
-				.replace(R.id.fragment_container, resultFragment)
-				.commit();
-	}
 	@Override
 	public void onBrocaIndexButtonClicked() {
 		getSupportFragmentManager().beginTransaction()
@@ -74,36 +59,13 @@ public class MainActivity extends AppCompatActivity implements MenuFragment.OnFr
 	}
 
 	@Override
-	public void onTryAgainButtonClicked(String tag) {
-		if (tag.equalsIgnoreCase("1")) {
-			getSupportFragmentManager().beginTransaction()
-					.replace(R.id.fragment_container, brocaIndexFragment)
-					.commit();
-		}else if(tag.equalsIgnoreCase("2")){
-			getSupportFragmentManager().beginTransaction()
-					.replace(R.id.fragment_container, bmiFragment)
-					.commit();
-		}
-	}
-
-	@Override
 	public void onBodyMassIndexButtonClicked() {
-		getSupportFragmentManager().beginTransaction()
-				.replace(R.id.fragment_container, bmiFragment)
-				.commit();
-	}
-
-	@Override
-	public void onPointerCaptureChanged(boolean hasCapture) {
 
 	}
 
 	@Override
-	public void onCalculateBmiIndexClicked(float index) {
-		resultFragment.setInformation("The Body Mass Index is   "+Math.round(index)+"kg/m2");
-		resultFragment.setStatus("2");
-		getSupportFragmentManager().beginTransaction()
-				.replace(R.id.fragment_container, resultFragment)
-				.commit();
+	public void onCalculateBrocaIndexClicked(float index) {
+
 	}
 }
+
